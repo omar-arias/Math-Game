6932c551d01fc7239bac7443b5f29c3559371235
Omar <omar.arias7@me.com>
2021-02-22 17:39:47 -0700

    A simple math game

---
+++ Math Game/src/Driver.java
@@ -0,0 +1,44 @@
+/*
+ * Omar Arias 
+ * Math Game
+ * A simple addition game
+ */
+
+import java.util.Scanner;
+
+public class Driver 
+{
+	static Scanner input;
+
+	public static void main(String[] args) 
+	{
+		input = new Scanner(System.in);
+		
+		int level;
+		
+		System.out.println("Select a level");
+		System.out.println("Level 1, 2 or 3?");
+		level = input.nextInt();
+	/*	
+		// numbers 0 - 10,
+		if (level == 1)
+		{
+			
+		}
+		
+		// numbers 0 - 100
+		if (level == 2)
+		{
+			
+		}
+		
+		// numbers 0 - 1000
+		if (level == 3)
+		{
+			
+		}
+		
+		*/
+	} // End main
+
+} // End Driver
