a20c0b9b4689544a208c92b20c9a777ef4458d5a
Omar <omar.arias7@me.com>
2021-03-01 15:10:01 -0700

    Update Driver.java
    Added to level 1 if statement.

--- Math Game/src/Driver.java
+++ Math Game/src/Driver.java
@@ -15,17 +15,42 @@ public class Driver
 		input = new Scanner(System.in);
 		
 		int level;
+		int firstNumber;
+		int secondNumber;
+		int min;
+		int max;
+		int response;
+		int answer;
 		
 		System.out.println("Select a level");
 		System.out.println("Level 1, 2 or 3?");
 		level = input.nextInt();
-	/*	
+
 		// numbers 0 - 10,
 		if (level == 1)
 		{
+			min = 0;
+			max = 10;
 			
+			firstNumber = (int) (Math.random() * (max - min + 1) + min);
+			secondNumber = (int) (Math.random() * (max - min + 1) + min);
+			answer = firstNumber + secondNumber;
+			
+			
+			System.out.println("Solve");
+			System.out.println(firstNumber + " + " + secondNumber);
+			response = input.nextInt();
+			
+			if (response == answer)
+			{
+				System.out.println("Correct! Well done!");
+			}
+			else
+			{
+				System.out.println("Oops! The correct answer is " + answer);
+			}
 		}
-		
+		/*
 		// numbers 0 - 100
 		if (level == 2)
 		{
@@ -38,7 +63,7 @@ public class Driver
 			
 		}
 		
-		*/
+	*/
 	} // End main
 
 } // End Driver
