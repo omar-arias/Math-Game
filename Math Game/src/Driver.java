fb8e764a943a986caa085c8ae3650f33b47bad26
Omar <omar.arias7@me.com>
2021-03-04 22:41:40 -0700

    Update Driver.java

--- Math Game/src/Driver.java
+++ Math Game/src/Driver.java
@@ -6,102 +6,68 @@
 
 import java.util.Scanner;
 
-public class Driver 
-{
+public class Driver {
 	static Scanner input;
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		input = new Scanner(System.in);
-		
+
 		int level;
-		int firstNumber;
-		int secondNumber;
-		int min;
-		int max;
-		int response;
-		int answer;
-		
+		int min = 0;
+		int max = 0;
+
+		String keepPlaying = "y";
+
 		System.out.println("Select a level");
 		System.out.println("Level 1 , 2 or 3?");
 		level = input.nextInt();
 
-		// numbers 0 - 10,
-		if (level == 1)
+		if (level == 1) // numbers 0 - 10
 		{
 			min = 0;
 			max = 10;
-			
-			firstNumber = (int) (Math.random() * (max - min + 1) + min);
-			secondNumber = (int) (Math.random() * (max - min + 1) + min);
-			answer = firstNumber + secondNumber;
-			
-			
-			System.out.println("Solve");
-			System.out.println(firstNumber + " + " + secondNumber + " =");
-			response = input.nextInt();
-			
-			if (response == answer)
-			{
-				System.out.println("Correct! Well done!");
-			}
-			else
-			{
-				System.out.println("Oops! The correct answer is " + answer);
-			}
 		}
-		
-		// numbers 0 - 100
-		if (level == 2)
+
+		if (level == 2) // numbers 0 - 100
 		{
 			min = 0;
 			max = 100;
-			
-			firstNumber = (int) (Math.random() * (max - min + 1) + min);
-			secondNumber = (int) (Math.random() * (max - min + 1) + min);
-			answer = firstNumber + secondNumber;
-			
-			
-			System.out.println("Solve");
-			System.out.println(firstNumber + " + " + secondNumber + " =");
-			response = input.nextInt();
-			
-			if (response == answer)
-			{
-				System.out.println("Correct! Well done!");
-			}
-			else
-			{
-				System.out.println("Oops! The correct answer is " + answer);
-			}
 		}
-		
-		// numbers 0 - 1000
-		if (level == 3)
+
+		if (level == 3) // numbers 0 - 1000
 		{
 			min = 0;
 			max = 1000;
-			
-			firstNumber = (int) (Math.random() * (max - min + 1) + min);
-			secondNumber = (int) (Math.random() * (max - min + 1) + min);
-			answer = firstNumber + secondNumber;
-			
-			
-			System.out.println("Solve");
-			System.out.println(firstNumber + " + " + secondNumber + " =");
-			response = input.nextInt();
-			
-			if (response == answer)
-			{
-				System.out.println("Correct! Well done!");
-			}
-			else
-			{
-				System.out.println("Oops! The correct answer is " + answer);
-			}
 		}
-		
+
+		playGame(min, max);
 	
+
 	} // End main
+
+	public static void playGame(int min, int max) 
+	{
+
+		int answer;
+		int firstNumber;
+		int secondNumber;
+		int response;
+
+		firstNumber = (int) (Math.random() * (max - min + 1) + min);
+		secondNumber = (int) (Math.random() * (max - min + 1) + min);
+		answer = firstNumber + secondNumber;
+
+		System.out.println("Solve");
+		System.out.println(firstNumber + " + " + secondNumber + " =");
+		response = input.nextInt();
+
+		if (response == answer) 
+		{
+			System.out.println("Correct! Well done!");
+		} else 
+			System.out.println("Oops! The correct answer is " + answer);
+		
+
+	} // End playGame
 
 } // End Driver
