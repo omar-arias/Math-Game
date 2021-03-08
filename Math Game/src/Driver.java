d32ee4cfc8666a69cb5c6652ac2ca9e2aab6c8e5
Omar <omar.arias7@me.com>
2021-03-07 23:26:00 -0700

    Added getLevel function

--- Math Game/src/Driver.java
+++ Math Game/src/Driver.java
@@ -7,17 +7,72 @@
 import java.util.Scanner;
 
 public class Driver {
+	
 	static Scanner input;
 
 	public static void main(String[] args) {
 		input = new Scanner(System.in);
 
+		char keepPlaying;
+		
+		do
+		{
+			playGame();
+			
+			System.out.println("\nWould you like to keep playing? Type:( y or n )");
+			keepPlaying = input.next().charAt(0);
+			
+		}while(keepPlaying == 'y');
+	
+		System.out.println("\nThanks for playing see you next time!");
+		input.close();
+	} // End main
+/**************************************************************************************
+ * 
+ */
+	public static void playGame() 
+	{
+		
+		int min;
+		int max;
+		int answer;
+		int number1;
+		int number2;
+		int response;
+		int [] numberArray = new int [2];
+		
+		getLevel(numberArray);
+		
+		min = numberArray[0];
+		max = numberArray [1];
+
+		number1 = (int) (Math.random() * (max - min + 1) + min);
+		number2 = (int) (Math.random() * (max - min + 1) + min);
+		answer = number1 + number2;
+
+		System.out.println("Solve");
+		System.out.println(number1 + " + " + number2 + " =");
+		response = input.nextInt();
+
+		if (response == answer) 
+		{
+			System.out.println("Well done, thats correct!");
+		} else 
+			System.out.println("Oops! The correct answer is " + answer);
+		
+
+	} // End playGame
+/****************************************************************************************	
+ * 
+ * @param numberArray
+ */
+	public static void getLevel (int numberArray [])
+	{
+		
 		int level;
 		int min = 0;
 		int max = 0;
-
-		String keepPlaying = "y";
-
+		
 		System.out.println("Select a level");
 		System.out.println("Level 1 , 2 or 3?");
 		level = input.nextInt();
@@ -39,35 +94,10 @@ public class Driver {
 			min = 0;
 			max = 1000;
 		}
-
-		playGame(min, max);
-	
-
-	} // End main
-
-	public static void playGame(int min, int max) 
-	{
-
-		int answer;
-		int firstNumber;
-		int secondNumber;
-		int response;
-
-		firstNumber = (int) (Math.random() * (max - min + 1) + min);
-		secondNumber = (int) (Math.random() * (max - min + 1) + min);
-		answer = firstNumber + secondNumber;
-
-		System.out.println("Solve");
-		System.out.println(firstNumber + " + " + secondNumber + " =");
-		response = input.nextInt();
-
-		if (response == answer) 
-		{
-			System.out.println("Correct! Well done!");
-		} else 
-			System.out.println("Oops! The correct answer is " + answer);
 		
-
-	} // End playGame
-
+		numberArray [0] = min;
+		numberArray [1]= max;
+		
+	} // End getLevel
+	
 } // End Driver
